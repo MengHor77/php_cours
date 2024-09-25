9ba8edfeae3a399a09cfeeac0f950760c1a65290
meng hor <piseth125125@gmail.com>
2024-09-25 15:36:33 +0700

    task: add returnValue.php

---
+++ function/returnValue.php
@@ -0,0 +1,15 @@
+
+<?php
+function sum($a1,$a2){
+    $result = $a1+$a2;
+    return $result;
+}
+
+$x=10;
+$y=20;
+echo sum($x,$y) ."</br>"; /// output : 30 
+echo $x ."</br>" ; // output 10
+echo $y; // output 20
+
+
+?>
