eb1838326d1b2a16b10e6cd9daa7c8a00bbc5ce1
meng hor <piseth125125@gmail.com>
2024-09-25 13:33:16 +0700

    fix: add construct

--- class-and-object/class.php
+++ class-and-object/class.php
@@ -18,8 +18,8 @@
 class Book {
 
     // member variable
-    public $price;
-    public $title;
+    private $price;
+    private $title;
 
     // member fionction 
     public function setPrice($p){
@@ -67,6 +67,66 @@ $math->getTitle();
 $physic->getPrice();
 $chemistry->getPrice();
 $math->getPrice();
+
+
+?>
+
+
+
+
+
+
+<?php
+
+
+// exampl about  consrtructor 
+class Book2{
+    
+
+     // member variable
+     private $price1;
+     private $title1;
+ 
+     public function __construct($p1, $t1){
+
+        $this->price1= $p1;
+        $this->title1 =$t1;
+        }
+     // member fionction 
+     public function setPrice($p1){
+         $this->price1 = $p1;
+     }
+     public function getPrice(){
+        echo  $this->price1 ."</br>";
+     }
+     public function setTitle($t1){
+         $this->title1= $t1;
+     }
+ 
+     public function getTitle(){
+         echo $this->title1 ."</br>";
+     }
+  
+    
+}
+
+// initialize constructor 
+$physic1  = new Book2( "physic for hight school ", 10);
+$chemistry1  = new Book2( "chemistry for hight school ", 100);
+$math1  = new Book2( "math for hight school ", 108);
+
+// get value
+
+
+$physic1->getPrice();
+$chemistry1->getPrice();
+$math1->getPrice(); 
+
+$physic1->getTitle();
+$chemistry1->getTitle();
+$math1->getTitle();
+
+
 
 
 ?>
